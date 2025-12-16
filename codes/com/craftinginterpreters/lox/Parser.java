d813a82b9f5cbb19e1a8697879143d5fdde3fb12
cikn23019 <cikn23019@g.nihon-u.ac.jp>
2025-12-16 10:18:43 +0900

    Parser completed

--- codes/com/craftinginterpreters/lox/Parser.java
+++ codes/com/craftinginterpreters/lox/Parser.java
@@ -16,6 +16,14 @@ class Parser {
     this.tokens = tokens;
   }
 
+    Expr parse() {
+    try {
+      return expression();
+    } catch (ParseError error) {
+      return null;
+    }
+  }
+
     private Expr expression() {
     return equality();
   }
@@ -92,6 +100,8 @@ class Parser {
       consume(RIGHT_PAREN, "Expect ')' after expression.");
       return new Expr.Grouping(expr);
     }
+
+    throw error(peek(), "Expect expression.");
   }
 
 
@@ -138,5 +148,28 @@ class Parser {
     Lox.error(token, message);
     return new ParseError();
   }
+
+    private void synchronize() {
+    advance();
+
+    while (!isAtEnd()) {
+      if (previous().type == SEMICOLON) return;
+
+      switch (peek().type) {
+        case CLASS:
+        case FUN:
+        case VAR:
+        case FOR:
+        case IF:
+        case WHILE:
+        case PRINT:
+        case RETURN:
+          return;
+      }
+
+      advance();
+    }
+  }
+
 
 }
